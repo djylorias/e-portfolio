ece4a46d53db5a933c34bc497bd48f3eb3f6a504
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-03-17 21:25:17 +0100

    React implementation

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react';
+
+/** @type {import('vite').UserConfig} */
+export default {
+	// active le plugin react
+	plugins: [react()],
+    base: "/e-portfolio",
+};
