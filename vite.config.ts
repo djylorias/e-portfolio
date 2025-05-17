dbe54b51e182c5095c26bdef23dea68998103173
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2025-05-17 08:43:11 +0200

    Delete React
    React don't have been installed correctly to use Vercel, re-installation incoming

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react';
-
-/** @type {import('vite').UserConfig} */
-export default {
-	// active le plugin react
-	plugins: [react()],
-    base: "/e-portfolio",
-};
